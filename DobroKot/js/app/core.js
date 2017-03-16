8110a2225c1cd6c73939e15e27b145e97094ae57
WAROBUSHEK <WAROBUSHEK@DESKTOP-CCM4S9I>
2017-03-16 19:26:39 +0300

    Add project files.

---
+++ DobroKot/js/app/core.js
@@ -0,0 +1,41 @@
+﻿var Core = {
+    // State
+    active: 'main',
+    board: null,
+    thread: null,
+    loading: false,
+    list: {
+        boards: [],
+        thread: []
+    },
+
+    // Actions
+    openBoard: function (name) {
+        var self = this;
+        self.showPage('load');
+        $.ajax({
+            type: 'GET',
+            url: 'http://dobrochan.com/' + name + '/index.json',
+            success: function (res) {
+                console.log(res);
+                // this.board = name;
+            },
+            error: function (err) {
+                console.log(err);
+                self.createError('Error!');
+            }
+        });
+    },
+
+    // Utility
+
+    createError: function (err) {
+        var msgBox = new Windows.UI.Popups.MessageDialog(err);
+        msgBox.showAsync();
+    },
+
+    showPage: function (pagename) {
+        $('.page-show').removeClass('page-show');
+        $('#' + pagename + '-page').addClass('page-show');
+    }
+};
---
+++ DobroKot/js/app/main_page.js
@@ -0,0 +1,32 @@
+﻿var MainPage = {
+    boardList: ['b', 'a', 'rf', 'vg', 'cr', 's'],
+
+    generateBlock: function (name) {
+        var block = '<div class="board-block js-board-block" data-name="' + name + '">/' + name + '/</div>';
+        return block;
+    },
+
+    generatePage: function () {
+        var self = this;
+        var pageHTML = this.boardList.map(function (addr) {
+            return self.generateBlock(addr);
+        });
+        return pageHTML.join('');
+    },
+
+    // Actions
+
+    openBoard: function() {
+        Core.openBoard($(this).data('name'));
+    },
+
+    Start: function () {
+        $('#main-page-content').html(this.generatePage());
+        $('.js-board-block').click(this.openBoard);
+    }
+};
+
+$(document).ready(function () {
+    MainPage.Start();
+});
+
